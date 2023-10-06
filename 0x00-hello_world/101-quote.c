7cd1fde5bdb011c0bd2c3566d8bf972e79a5f357
Aymane <aymane.lamarti.8@gmail.com>
2023-10-06 13:26:21 +0000

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
+ * Main - Entry point
+ *
  * customPrint - Prints a string to the standard output.
  *
  * @str: The input string to be printed.
  */
-
 void customPrint(const char *str)
 {
 	while (*str != '\0')
@@ -16,9 +17,11 @@ void customPrint(const char *str)
 	putchar('\n');
 
 }
+
 int main(void)
 {
 	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
 	customPrint(message);
 	return (1);
 }

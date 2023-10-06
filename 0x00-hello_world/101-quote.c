1da96ae039d3d47344d1822a92f7c454e4595fc9
Aymane <aymane.lamarti.8@gmail.com>
2023-10-06 13:53:49 +0000

    putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -23,8 +23,7 @@ void customPrint(const char *str)
 
 int main(void)
 {
-	const char *str = "and that piece of art is useful\" - Dora Korpar, \
-			   2015-10-19";
+	const char *str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
 	customPrint(str);
 	return (1);

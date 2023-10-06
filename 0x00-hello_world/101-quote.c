a87628f7989d60b31985975d7b2f528b7e6bd9a7
Aymane <aymane.lamarti.8@gmail.com>
2023-10-06 10:39:32 +0000

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+void custumPrint(const char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str);
+	}
+}
+
+int main(void)
+{
+	const char *message = "and that piece of art is useful" - Dora Korpar, 2015-10-19"
+	customPrint(message);
+	return 1;
+}
+

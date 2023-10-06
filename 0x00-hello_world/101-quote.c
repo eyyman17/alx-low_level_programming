c16ad48c412d5cfebc0e026c5f386a8afff09070
Aymane <aymane.lamarti.8@gmail.com>
2023-10-06 12:53:39 +0000

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -15,7 +15,7 @@ void customPrint(const char *str)
 
 int main(void)
 {
-	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+	const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 	customPrint(message);
 	return 1;
 }

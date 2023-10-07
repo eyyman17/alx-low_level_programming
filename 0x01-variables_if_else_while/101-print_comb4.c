f6a334517aaf52181dd7328bc117bbffca870836
Aymane <aymane.lamarti.8@gmail.com>
2023-10-07 02:04:50 +0000

    comb3

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+
+	while (i <= 9)
+	{
+		for (j = 0 ; j < 10 ; j++)
+		{
+			if (i != j != k && i < j && j < k)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(k + '0');
+				if (i + j + k != 24)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}

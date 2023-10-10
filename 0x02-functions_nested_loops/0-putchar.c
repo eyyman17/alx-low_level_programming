c66ae303875aa0b397844d87188fc99875222c39
Aymane <aymane.lamarti.8@gmail.com>
2023-10-10 23:19:17 +0000

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point.
+ * main - Entry point
  *
- * Return: 0 for success.
+ * Description: this code prints -putchar
+ *
+ * Return : Always 0 when success
  */
 
 int main(void)
 {
-	printf("_putchar\n");
+	char str[] = "_putchar";
+	int ch;
+
+	for (ch = 0; ch < 8; ch++)
+		_putchar(str[ch]);
+	_putchar('\n');
+
 	return (0);
 }

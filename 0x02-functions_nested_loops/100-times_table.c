2139283223d3423be11a76b497679dddbcf449dc
Aymane <aymane.lamarti.8@gmail.com>
2023-10-11 15:42:12 +0000

    m

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,8 +17,7 @@ void print_times_table(int n)
 	if (n >= 0 && n <= 15)
 		for (i = 0; i <= n; i++)
 		{
-			_putchar('0');
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				m = i * j;
 				_putchar(',');

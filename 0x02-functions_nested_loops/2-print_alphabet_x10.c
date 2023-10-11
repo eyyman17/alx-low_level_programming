25b61bb6ad49adac47882afe9425d065854b7d93
Aymane <aymane.lamarti.8@gmail.com>
2023-10-11 00:28:51 +0000

    alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * print_alphabet_x10 - Prints alphabets from a to z 10 times
+ *
+ * return: always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int ch;
+	int t;
+
+	for (t = 0; t < 10; t++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+	}
+}

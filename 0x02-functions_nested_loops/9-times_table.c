745af09d958ab6f8c473a8f2a1d9589d274d444e
Aymane <aymane.lamarti.8@gmail.com>
2023-10-11 02:57:32 +0000

    timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -27,9 +27,9 @@ void times_table(void)
 				_putchar((r / 10) + '0');
 				_putchar((r % 10) + '0');
 			}
-			_putchar(',');
-			_putchar(' ');
 		}
+		_putchar(',');
+		_putchar(' ');
 		_putchar('\n');
 	}
 }

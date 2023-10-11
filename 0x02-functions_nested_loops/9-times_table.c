33a3e3320c095fd8b1bea684b2f1611ddaddd705
Aymane <aymane.lamarti.8@gmail.com>
2023-10-11 02:48:26 +0000

    timetable

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table, starting with 0.
+ *
+ * Return: Always 0 (Success).
+ */
+
+void times_table(void)
+{
+	int m;
+	int n;
+
+	for (m = 0; m < 10; m++)
+	{
+		for (n = 0; n < 10; n++)
+		{
+			int r = n * m;
+			if (r < 10)
+			{
+				_putchar(r + '0');
+			}
+			else
+			{
+				_putchar((r / 10) + '0');
+				_putchar((r % 10) + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}

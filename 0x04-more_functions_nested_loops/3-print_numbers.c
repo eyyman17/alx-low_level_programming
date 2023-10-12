e0a0c617021880c974a2cf14d7bdbacc42498c18
Aymane <aymane.lamarti.8@gmail.com>
2023-10-12 23:40:00 +0000

    0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9.
+ *
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar('\n');
+}

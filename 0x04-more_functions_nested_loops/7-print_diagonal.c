fb4ba880a42f5a0f307839074f5f3493100741f7
Aymane <aymane.lamarti.8@gmail.com>
2023-10-13 13:45:16 +0000

    diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,9 +14,13 @@ void print_diagonal(int n)
 	if (n > 0)
 		for (i = 1; i <= n; i++)
 		{
-			for (j = 2; j <= n; j++)
+			for (j = 2; j <= i; j++)
+			{
 				_putchar(' ');
+			}
 			_putchar('\\');
+			_putchar('\n');
 		}
+	else
 	_putchar('\n');
 }

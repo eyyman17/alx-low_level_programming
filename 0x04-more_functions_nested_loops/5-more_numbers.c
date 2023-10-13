d04ed9025b1c51718c6e0a2e95dda78ea2c1772c
Aymane <aymane.lamarti.8@gmail.com>
2023-10-13 01:04:52 +0000

    numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,14 +9,20 @@ void more_numbers(void)
 {
 	int i;
 	int j;
+	int num;
 
 	for (i = 0; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(j + '0');
+			num = j;
+			if (j > 9)
+			{
+				_putchar(1 + 48);
+				num = j % 10;
+			}
+			_putchar(j + 48);
 		}
 		_putchar('\n');
 	}
-	_putchar('\n');
 }

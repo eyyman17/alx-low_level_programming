8c57a28bd2632260a531be3ac83628051278cce5
Aymane <aymane.lamarti.8@gmail.com>
2023-10-13 00:21:33 +0000

    numberz

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints from 0 to 14, 10 times.
+ *
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j + '0');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}

d420be5deee7ff65a5ad4ece811819a3ad4bc527
Aymane <aymane.lamarti.8@gmail.com>
2023-10-13 13:29:35 +0000

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a straight line.
+ *
+ * @n: integer
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n > 0)
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 2; j <= n; j++)
+				_putchar(' ');
+			_putchar('\');
+		}
+	_putchar('\n');
+}

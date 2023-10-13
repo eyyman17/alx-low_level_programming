e387bd2bbd349b0573cc495dfa54cd262303f2c7
Aymane <aymane.lamarti.8@gmail.com>
2023-10-13 17:28:40 +0000

    fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,6 +12,7 @@ int main(void)
 	int i;
 
 	for (i = 1; i <= 100; i++)
+	{
 		if ((i % 3 == 0) && !(i % 5 == 0))
 			printf("Fizz");
 		else if ((i % 5 == 0) && !(i % 3 == 0))
@@ -25,6 +26,6 @@ int main(void)
 			printf(" ");
 		else
 			printf("\n");
-
+	}
 	return (0);
 }

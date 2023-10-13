01a83f7a21353f0825db593f46ddab3426c8b516
Aymane <aymane.lamarti.8@gmail.com>
2023-10-13 17:08:02 +0000

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - entry point.
+ *
+ */
+
+void main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+		if (i % 3 == 0)
+			printf("Fizz ");
+		else if (i % 5 == 0)
+			printf("Buzz ");
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("FizzBuzz ");
+		else
+			printf("%d ", i);
+}

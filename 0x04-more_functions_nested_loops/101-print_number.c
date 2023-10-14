aee751f14c5cd92c2e8c8f88e439b795b11a84ec
Aymane <aymane.lamarti.8@gmail.com>
2023-10-14 02:05:31 +0000

    printnum

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+	{
+		m = n;
+	}
+	if (m / 10)
+	{
+		print_number(m /10);
+	}
+	_putchar((n1 % 10) + 48);
+}

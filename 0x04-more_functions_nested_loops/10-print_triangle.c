0f474a566fd46ecc03753317b81f813d055e168c
Aymane <aymane.lamarti.8@gmail.com>
2023-10-14 00:52:08 +0000

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a square.
+ *
+ * @size: the size of the triangle.
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size > 0)
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				if (i + j <= size)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+		}
+	else
+	_putchar('\n');
+}

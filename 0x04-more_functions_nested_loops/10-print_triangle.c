d2deb584fe7d44442e005092f54b10fe2966bdc6
Aymane <aymane.lamarti.8@gmail.com>
2023-10-14 00:58:05 +0000

    triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,16 +12,19 @@ void print_triangle(int size)
 	int j;
 
 	if (size > 0)
+	{
 		for (i = 1; i <= size; i++)
 		{
 			for (j = 1; j <= size; j++)
 			{
-				if (i + j <= size)
+				if ((i + j) <= size)
 					_putchar(' ');
 				else
 					_putchar('#');
 			}
+			_putchar('\n');
 		}
+	}
 	else
 	_putchar('\n');
 }

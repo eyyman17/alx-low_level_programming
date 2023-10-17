a6857bd4fbcd15a9bb0f27eef83843ce3cdabded
Aymane <aymane.lamarti.8@gmail.com>
2023-10-17 21:38:58 +0000

    pointers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ int _atoi(char *s)
 		if (*s == '-')
 			sign *= -1;
 		else if (*s >= '0' && *s <= '9')
-			result = (result * 10) + (*s + '0');
+			result = (result * 10) + (*s - '0');
 		else if (result > 0)
 			break;
 	} while (*s++);

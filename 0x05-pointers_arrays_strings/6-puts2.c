19bcb2dd74101fe9cdcee5cbf54da7a026ff364f
Aymane <aymane.lamarti.8@gmail.com>
2023-10-17 01:11:40 +0000

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: String
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	for (len = 0; str[len] != '\0'; len++)
+		;
+
+	for (i = 0; i <= len; i = i + 2)
+	{
+		_putchar('str[i]');
+		-putchar('\n');
+	}
+}

3b00cf8deeed6dc3fd8478e4ee0d76bb2016e601
Aymane <aymane.lamarti.8@gmail.com>
2023-10-21 01:35:49 +0000

    pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ *
+ * @str: a string.
+ *
+ * Return: Always 0.
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		while (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (str[i - 1] == ' ' ||
+			str[i - 1] == '\t' ||
+			str[i - 1] == '\n' ||
+			str[i - 1] == ',' ||
+			str[i - 1] == ';' ||
+			str[i - 1] == '.' ||
+			str[i - 1] == '!' ||
+			str[i - 1] == '?' ||
+			str[i - 1] == '"' ||
+			str[i - 1] == '(' ||
+			str[i - 1] == ')' ||
+			str[i - 1] == '{' ||
+			str[i - 1] == '}' ||
+			i == 0)
+				str[i] = str[i] - 32;
+			index++;
+		}
+	}
+	return (str);
+}

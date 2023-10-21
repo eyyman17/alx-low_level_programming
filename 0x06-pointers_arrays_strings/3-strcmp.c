da19116dfc5ecec704eaeff0f3d2ed23bdfd71cb
Aymane <aymane.lamarti.8@gmail.com>
2023-10-21 00:04:54 +0000

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,16 +12,17 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i = 0;
 
-	for (i = 0; s1[i] != '\0'; i++)
-		;
-	for (j = 0; s2[j] != '\0'; j++)
-		;
-	if (i < j)
-		return (-15);
-	else if (i > j)
-		return (15);
-	else
-		return (0);
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			i = ((int)*s1) - ((int)*s2);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (i);
 }

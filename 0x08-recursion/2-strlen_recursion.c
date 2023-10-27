727c1424068280770a6b71da0071922bde5a6e67
Aymane <aymane.lamarti.8@gmail.com>
2023-10-27 00:45:40 +0000

    recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,10 @@
 
 int _strlen_recursion(char *s)
 {
-	int i;
-
 	if (*s == '\0')
 		return (0);
 	else
 	{
-		return (1 + _strlen_recursion(char *s));
+		return (1 + _strlen_recursion(s + 1));
 	}
 }

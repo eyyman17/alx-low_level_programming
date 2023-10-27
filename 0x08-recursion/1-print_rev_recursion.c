bca7f071d15dbaffbac6ffeb23b6d3b605133363
Aymane <aymane.lamarti.8@gmail.com>
2023-10-27 00:22:02 +0000

    recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints string in reverse
+ *
+ * @s: string.
+ *
+ * Return: Always 0.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(*s++);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}

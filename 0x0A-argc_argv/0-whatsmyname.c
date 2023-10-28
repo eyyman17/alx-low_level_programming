108f0f0162db7859280c6ca0a61b6cdf9c58df0a
Aymane <aymane.lamarti.8@gmail.com>
2023-10-28 01:07:20 +0000

    argc,argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: argument counter
+ * @argv: arguments in a string
+ *
+ * Return: when success 0.
+ */
+
+int main(int argc, char argv[])
+{
+	printf("%d \n", argv[0]);
+}

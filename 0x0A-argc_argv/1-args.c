dc4e520662be1f6f423c909776f8d9fccb124110
Aymane <aymane.lamarti.8@gmail.com>
2023-10-28 01:45:04 +0000

    argc, argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <main.h>
+
+
+/**
+ * main - entry point
+ *
+ * @argc: arguments counter
+ * @argv: argument passed.
+ *
+ * Return: 0.
+ */
+
+int main(int argc, char *argv)
+{
+	(void)*argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}

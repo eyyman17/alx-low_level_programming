7fee516d04bb57b637410a198916c7d08b040619
Aymane <aymane.lamarti.8@gmail.com>
2023-11-01 01:31:07 +0000

    malloc

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,12 +17,17 @@ char *_strdup(char *str)
 
 	if (str == NULL)
 		return (NULL);
+
 	while (str[len] != '\0')
 		len++;
-	strcpy = malloc(len * sizeof(char));
+	strcpy = malloc((len + 1) * sizeof(char));
+
 	if (strcpy == NULL)
 		return (NULL);
+
 	for (i = 0; i < len; i++)
 		strcpy[i] = str[i];
+	strcpy[len] = '\0';
+
 	return (strcpy);
 }

55bc88b58a0c0fdf9cfa87b35034fd6f2c001a71
Aymane <aymane.lamarti.8@gmail.com>
2023-11-01 00:55:25 +0000

    malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,12 +12,12 @@
 
 char *create_array(unsigned int size, char c)
 {
-	int i;
+	unsigned int i;
 	char *s;
 
 	if (size == 0)
 		return (NULL);
 	s = malloc(size * sizeof(char));
 	for (i = 0; i <= size; i++)
-		s[i] = s;
+		s[i] = c;
 }

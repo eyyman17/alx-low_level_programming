4a05c0e98826439d70715e9fcc39986452bf19d3
Aymane <aymane.lamarti.8@gmail.com>
2023-11-03 01:42:29 +0000

    more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,8 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i, len1, len2;
 	unsigned int j;
 
+	len1 = 0;
 	while (s1[len1] != '\0')
 		len1++;
+	len2 = 0;
 	while (s2[len2] != '\0')
 		len2++;
 

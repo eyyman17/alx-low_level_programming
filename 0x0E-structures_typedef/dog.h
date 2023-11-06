bb729e46ab74ed7a404cd07bfe111dfa4276153b
Aymane <aymane.lamarti.8@gmail.com>
2023-11-06 22:50:44 +0000

    deftype

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -6,18 +6,22 @@
  * @name: pointer
  * @owner: pointer
  * @age: float
+ *
+ * @dog_t: new name for struct dog
  **/
 
-struct dog
+deftype
 {
-	char *name;
-	char *owner;
-	float age;
-};
+	struct dog
+	{
+		char *name;
+		char *owner;
+		float age;
+	} dog_t;
+}
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
 
 
 

71197710e60ee345ee09b57ca40278abf9451a28
Aymane <aymane.lamarti.8@gmail.com>
2023-11-06 22:46:56 +0000

    struct

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,6 +15,6 @@ void print_dog(struct dog *d)
 		return;
 
 	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
-	printf("Age: %.1f\n", d->age >= 0 ? d->age : -1.0);
+	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", (d->owner != NULL) ? d-> owner : "(nil)");
 }

196d3c84dc32106223d0e3231ecd1458e0227bf1
Aymane <aymane.lamarti.8@gmail.com>
2023-11-06 01:00:42 +0000

    preprocessor

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,13 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-#define ABS(x) x
-
-int main(void)
-{
-#if ABS(x) < 0
-	x = (-x)
-#endif
-}
+#define ABS(x) ((x) < 0 ? -(x) : (x))
 
 #endif

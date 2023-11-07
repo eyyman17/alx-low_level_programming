228cfbee3a7f3851f54adbd769832776451de042
Aymane <aymane.lamarti.8@gmail.com>
2023-11-07 00:22:21 +0000

    struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,11 +20,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (!name || age <= 0 || !owner)
 		return (NULL);
-
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
 		return (NULL);
-
 	while (name[len1] != '\0')
 		len1++;
 	dog->name = malloc(sizeof(char) * (len1 + 1));
@@ -33,7 +31,6 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
 	while (owner[len2] != '\0')
 		len2++;
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
@@ -43,18 +40,12 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(dog);
 		return (NULL);
 	}
-
 	for (i = 0; name[i]; i++)
-	{
 		dog->name[i] = name[i];
-		dog->name[i] = '\0';
-	}
+	dog->name[i] = '\0';
 	dog->age = age;
-
 	for (j = 0; owner[j]; j++)
-	{
 		dog->owner[j] = owner[j];
-		dog->owner[j] = '\0';
-	}
+	dog->owner[j] = '\0';
 	return (dog);
 }

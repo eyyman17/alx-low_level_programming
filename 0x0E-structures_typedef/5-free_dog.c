c415e06d0c3dda2e3b04e58a8c3e20075d46a04c
Aymane <aymane.lamarti.8@gmail.com>
2023-11-07 00:25:44 +0000

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: pointer
+ *
+ **/
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}

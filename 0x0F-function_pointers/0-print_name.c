d13bcbd5d7454a2b6d36c79095a4b85e8bd6cd73
Aymane <aymane.lamarti.8@gmail.com>
2023-11-09 01:13:12 +0000

    Function Pointers

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,12 @@
  * print_name - function that prints a name.
  *
  * @name: name
- * @(*f)(char *): function pointer.
+ * @f: function pointer.
  *
  * Return: Nothing.
  **/
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	f(char *name);
 }

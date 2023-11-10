b96edf986dc85e7b7877733ba02ea5cf459da38d
Aymane <aymane.lamarti.8@gmail.com>
2023-11-10 00:38:13 +0000

    calc

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,14 +19,14 @@ int main(int argc, char *argv[])
 		b = atoi(argv[3]);
 		op_func = get_op_func(argv[2]);
 		if (!op_func)
-			printf("Error\n");
+			printf("Error\n"), exit(97);
 		if (!b && (argv[2][0] == '/' || argv[2][0] == '%'))
-			printf("Error\n");
+			printf("Error\n"), exit(59);
 
 		printf("%d\n", op_func(a, b));
 		return (0);
 	}
 	else
-		printf("Error\n");
+		printf("Error\n"), exit(98);
 	return (0);
 }

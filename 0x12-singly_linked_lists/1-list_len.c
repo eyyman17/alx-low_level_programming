cf6e87caca57be23d257231046c39bda1cead75e
Aymane <aymane.lamarti.8@gmail.com>
2023-11-19 19:50:47 +0000

    lists

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * list_len - returns the number of elemets in a linked list_t list
+ *
+ * @h: the list_t
+ *
+ * Return: the number of elements
+ **/
+
+size_t list_len(const list_t *h)
+{
+	const list_t *current;
+	int i = 0;
+
+	while (current != NULL)
+		i++;
+	return (i);
+}

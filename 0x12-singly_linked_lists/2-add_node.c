68a8f5c5581442ca17443cbea727531ea798961a
Aymane <aymane.lamarti.8@gmail.com>
2023-11-19 21:06:32 +0000

    lists:

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -18,11 +18,12 @@ list_t *add_node(list_t **head, const char *str)
 	node_to_add = malloc(sizeof(list_t));
 	if (node_to_add == NULL)
 		return (NULL);
-	
+
 	strd = strdup(str);
 
 	node_to_add->str = strd;
 	node_to_add->next = *head;
+	node_to_add->len = strlen(strd);
 	*head = node_to_add;
 
 	return (*head);

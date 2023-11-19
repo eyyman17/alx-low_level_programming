f1a1c9a644b2b31199a0b8bdce4718c4b728a6f5
Aymane <aymane.lamarti.8@gmail.com>
2023-11-19 01:00:58 +0000

    lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,16 +12,16 @@ size_t print_list(const list_t *h)
 {
 	const list_t *current = h;
 	int i = 0;
-	
+
 	while (current != NULL)
 	{
 		if (current->str == NULL)
-			printf("[0] (nil)");
+			printf("[0] (nil)\n");
 		else
 			printf("[%d] %s\n", current->len, current->str);
 		current = current->next;
 		i++;
 	}
-	
+
 	return (i);
 }

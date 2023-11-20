82df14335c8c29e6f01338d4011524a5a4bc269d
Aymane <aymane.lamarti.8@gmail.com>
2023-11-20 22:03:40 +0000

    lists

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,17 +8,17 @@
  * Return: Nothing.
  **/
 
-void free_listint2(listint_t *head)
+void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = head;
+	temp = *head;
 	while (temp != NULL)
 	{
-		temp = head;
+		temp = *head;
 		temp = temp->next;
-		free(head);
-		head = temp;
+		free(*head);
+		*head = temp;
 	}
-	head = NULL;
+	*head = NULL;
 }

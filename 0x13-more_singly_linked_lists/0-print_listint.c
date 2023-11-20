01b0458931cf674666a2fa817e19af7c0f4f38b4
Aymane <aymane.lamarti.8@gmail.com>
2023-11-20 20:18:44 +0000

    lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,7 +15,7 @@ size_t print_listint(const listint_t *h)
 
 	while (current != NULL)
 	{
-		printf("%d", current->n);
+		printf("%d\n", current->n);
 		current = current->next;
 		count++;
 	}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists"
+
+/**
+ * add_nodeint - adds new node at the beginning
+ *
+ * @head: pointer to pointer to head of the list
+ * @n: const integer
+ *
+ * Return: the address of the new element, or NULL if failed
+ **/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+

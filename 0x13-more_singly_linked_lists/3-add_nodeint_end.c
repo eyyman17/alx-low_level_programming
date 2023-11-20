03031648ea50e9969fb3c52bd323808171e6b2d3
Aymane <aymane.lamarti.8@gmail.com>
2023-11-20 21:37:25 +0000

    lists

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * add_modeint_end - add a new node at the end of the list
+ *
+ * @head: head of list
+ * @n: value of node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ **/
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new, ptr;
+
+	new = malloc (sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	ptr = *head;
+	while (ptr->next != NULL)
+		ptr = ptr->next;
+	ptr->next = new;
+
+	return (new);
+}

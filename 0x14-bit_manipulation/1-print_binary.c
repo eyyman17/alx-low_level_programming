08693f03f44e2ac581ec9000b38ec5341d712f8a
Aymane <aymane.lamarti.8@gmail.com>
2023-11-25 02:21:39 +0000

    bit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -11,16 +11,22 @@
 void print_binary(unsigned long int n)
 {
 	unsigned long int m = 1UL;
+	int i = 0;
 
 	m = m << (sizeof(unsigned long int) * 8 - 1);
 
 	while (m > 0)
 	{
 		if (n & m)
+		{
 			_putchar('1');
-		else
+			i = 1;
+		}
+		else if (i || m == 1)
 			_putchar('0');
 
 		m >>= 1;
 	}
+	if (!i)
+		_putchar('0');
 }

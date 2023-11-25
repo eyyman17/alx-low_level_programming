d52ee20cc9b191c8c1725e1126057165977ae3cf
Aymane <aymane.lamarti.8@gmail.com>
2023-11-25 02:49:11 +0000

    bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,7 +14,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n |= (0UL << index);
+	*n &= ~(1UL << index);
 
 	return (1);
 }

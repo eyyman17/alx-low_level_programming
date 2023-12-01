0b7cde60200459d5df03e34f9738c24b5f4ba202
Aymane <aymane.lamarti.8@gmail.com>
2023-12-01 00:44:30 +0000

    file_d

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,16 +3,18 @@
 /*
  * cp_file - Copies the content of a file to another file
  *
+ * argc: argument count
+ * argv: argument vectors
  *
- * Return: 0 on Success.
+ * Return: Nothing.
  **/
 
 
-int cp_file(int argc, char *argv[])
+void cp_file(int argc, char *argv[])
 {
 	int file_d1, file_d2;
-	char buffer[1024], buffer2[];
-	ssize_t bytesRead1, bytesRead2, write_check;
+	char buffer[1024];
+	ssize_t bytesRead;
 	char *file_from = argv[1];
 	char *file_to = argv[2];
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
@@ -29,63 +31,48 @@ int cp_file(int argc, char *argv[])
 		dprintf(2, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
-	while ((bytesRead1 = read(file_d1, buffer, sizeof(buffer))) > 0)
-	{
-		buffer2 += buffer;
-	}
-	if (bytesRead1 == -1)
-	{
-		close(file_d1);
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
 
 	file_d2 = open(file_to, O_WRONLY | O_CREAT, mode);
 	if (file_d2 == -1)
 	{
-		dprint(2, "Error: Can't write to %s\n", file_to);
+		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
 
 	if (ftruncate(file_d2, 0) == -1)
 	{
 		close(file_d2);
-		dprint(2, "Error: Can't write to %s\n", file_to);
-                exit(99);
-	}
-
-	write_check = write(file_d2, buffer2, sizeof(buffer));
-	if (write_check == -1)
-	{
-		close(file_d2);
-		dprint(2, "Error: Can't write to %s\n", file_to);
+		dprintf(2, "Error: Can't write to %s\n", file_to);
 		exit(99);
 	}
+	while ((bytesRead = read(file_d1, buffer, sizeof(buffer))) > 0)
+	{
+		if (write(file_d2, buffer, bytesRead) == -1)
+		{
+			close(file_d1);
+			close(file_d2);
+			dprintf(2, "Error: Can't write to %s\n", file_to);
+			exit(99);
+		}
+	}
+        if (bytesRead == -1)
+        {
+		close(file_d1);
+		close(file_d2);
+		dprintf(2, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
+
+
 
 	if (close(file_d1) == -1)
 	{
-		dprint(2, "Error: Can't close fd %d\n", file_d1);
+		dprintf(2, "Error: Can't close fd %d\n", file_d1);
                 exit(100);
 	}
 	if (close(file_d2) == -1)
 	{
-		dprint(2, "Error: Can't close fd %d\n", file_d2);
+		dprintf(2, "Error: Can't close fd %d\n", file_d2);
 		exit(100);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}

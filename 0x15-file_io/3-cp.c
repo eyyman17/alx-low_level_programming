0d73df7736eb6a0954f53b4f97d43e5e729bbe09
Aymane <aymane.lamarti.8@gmail.com>
2023-12-01 01:07:27 +0000

    file_d

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,54 +20,25 @@ int main(int argc, char *argv[])
 	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
-	{
-		dprintf(2, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
 	file_d1 = open(file_from, O_RDONLY);
 	if (file_d1 == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
 
 	file_d2 = open(file_to, O_WRONLY | O_CREAT, mode);
 	if (file_d2 == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", file_to);
-		exit(99);
-	}
+		dprintf(2, "Error: Can't write to %s\n", file_to), exit(99);
 
 	while ((bytesRead = read(file_d1, buffer, sizeof(buffer))) > 0)
-	{
 		if (write(file_d2, buffer, bytesRead) == -1)
-		{
-			close(file_d1);
-			close(file_d2);
-			dprintf(2, "Error: Can't write to %s\n", file_to);
-			exit(99);
-		}
-	}
+			dprintf(2, "Error: Can't write to %s\n", file_to), exit(99);
         if (bytesRead == -1)
-        {
-		close(file_d1);
-		close(file_d2);
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
-
-
+		dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
 
 	if (close(file_d1) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_d1);
-                exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_d1), exit(100);
 	if (close(file_d2) == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", file_d2);
-		exit(100);
-	}
+		dprintf(2, "Error: Can't close fd %d\n", file_d2), exit(100);
 	return (0);
 }

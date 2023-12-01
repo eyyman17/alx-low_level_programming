ac7b65e5352cf9214a274b2d1510f6f6902bdf3b
Aymane <aymane.lamarti.8@gmail.com>
2023-12-01 01:12:15 +0000

    file_d

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -22,9 +22,9 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 		dprintf(2, "Usage: cp file_from file_to\n"), exit(97);
 
-	file_d1 = open(argv[1], O_RDONLY);
-	if (argv[1] == NULL || file_d1 == -1)
-		dprintf(2, "Error: Can't read from file %s\n", file_from), exit(98);
+	file_d1 = open(file_from, O_RDONLY);
+	if (file_from == NULL || file_d1 == -1)
+		dprintf(1, "Error: Can't read from file %s\n", file_from), exit(98);
 
 	file_d2 = open(file_to, O_WRONLY | O_CREAT, mode);
 	if (file_d2 == -1)

dea644c401285e14486551540d9a38a0720996b1
Aymane <aymane.lamarti.8@gmail.com>
2024-01-11 18:27:41 +0000

    insert

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -14,18 +14,40 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *ptr = *h, *new;
 	unsigned int i;
 
-	for (i = 0; i < (idx - 1); i++)
-	{
-		if (ptr == NULL)
-			return (NULL);
-		ptr = ptr->next;
-	}
+	if (*h == NULL)
+		return (NULL);
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
+
+	if (idx == 0)
+	{
+		new->next = *h;
+		new->prev = NULL;
+		if (*h != NULL)
+			(*h)->prev = new;
+		*h = new;
+		return (new);
+	}
+
+	for (i = 0; i < (idx - 1); i++)
+	{
+		if (ptr == NULL)
+			return (NULL);
+		ptr = ptr->next;
+	}
+	if (ptr == NULL)
+	{
+		new->next = NULL;
+		new->prev = *h;
+		if (*h != NULL)
+			(*h)->next = new;
+		return (new);
+	}
+
 	new->next = ptr->next;
 	new->prev = ptr;
 	ptr->next = new;

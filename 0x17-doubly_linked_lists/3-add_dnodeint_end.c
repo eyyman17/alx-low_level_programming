f92367ef0b644eb4c4a6bc80cff6f81ff34592e4
Aymane <aymane.lamarti.8@gmail.com>
2024-01-11 17:29:31 +0000

    new node

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -11,7 +11,7 @@
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
 	dlistint_t *new = NULL, *ptr = *head;
-	
+
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);

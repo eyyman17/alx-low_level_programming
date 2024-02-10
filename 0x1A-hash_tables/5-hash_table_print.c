838719cb0b68385f116de582bf9fac5b8c705a11
Aymane <aymane.lamarti.8@gmail.com>
2024-02-10 20:32:38 +0000

    print hash

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -28,7 +28,7 @@ void hash_table_print(const hash_table_t *ht)
 			printf("'%s': '%s'", node->key, node->value);
 			node = node->next;
 			if (node != NULL)
-				print(", ");
+				printf(", ");
 		}
 	}
 	printf("}\n");

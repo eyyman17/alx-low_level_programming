e4d9ca9359ec4ac9cd254b46560ae40dae076652
Aymane <aymane.lamarti.8@gmail.com>
2024-02-10 14:26:57 +0000

    hash

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -8,27 +8,13 @@
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-	hash_table_t *table;
 	unsigned long int hash;
 	int c;
-
-	table = malloc(sizeof(hash_table_t));
-	if (table = NULL)
-		return (NULL);
 
 	hash = 5381;
 	while ((c = *str++))
 	{
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	}
-
-	table->size = hash;
-	table->array = calloc(hash, sizeof(hash_node_t *));
-	if (table->array == NULL)
-	{
-		free(table);
-		return (NULL);
-	}
-
 	return (hash);
 }

5a7961253f806c771fe8ff7b6d2e5239a53cc206
Aymane <aymane.lamarti.8@gmail.com>
2024-02-10 20:52:55 +0000

    h

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -9,20 +9,24 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int i;
+	unsigned long int i, printed;
 	hash_node_t *node;
 
+	if (ht == NULL)
+		return;
 
 	printf("{");
+	printed = 0;
 	for (i = 0; ht->size; i++)
 	{
 		node = ht->array[i];
 		while (node != NULL)
 		{
+			if (printed)
+				printf(", ");
 			printf("'%s': '%s'", node->key, node->value);
 			node = node->next;
-			if (node != NULL)
-				printf(", ");
+			printed = 1;
 		}
 	}
 	printf("}\n");

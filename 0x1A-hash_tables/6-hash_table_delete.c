ccfde7f3bbefae1fbf7178d496fd7019b2abd448
Aymane <aymane.lamarti.8@gmail.com>
2024-02-10 20:12:10 +0000

    delete

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - a function that deletes a hash table.
+ * @ht: hash table to be deleted
+ *
+ * Return: Nothing
+ **/
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_table_t *head = ht;
+	hash_node_t *node;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				free(node->key);
+				free(node->value);
+				free(node);
+			}
+		}
+	}
+	free(head->array);
+	free(head);
+}

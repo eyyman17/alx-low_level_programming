52b70fa9e26f25662d2f67364eb0b6041312aac9
Aymane <aymane.lamarti.8@gmail.com>
2024-02-10 21:07:58 +0000

    delete

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -11,7 +11,7 @@ void hash_table_delete(hash_table_t *ht)
 {
 	unsigned long int i;
 	hash_table_t *head = ht;
-	hash_node_t *node;
+	hash_node_t *node, *tmp_node;
 
 	for (i = 0; i < ht->size; i++)
 	{
@@ -20,9 +20,11 @@ void hash_table_delete(hash_table_t *ht)
 			node = ht->array[i];
 			while (node != NULL)
 			{
+				tmp_node = node->next;
 				free(node->key);
 				free(node->value);
 				free(node);
+				node = tmp_node;
 			}
 		}
 	}
